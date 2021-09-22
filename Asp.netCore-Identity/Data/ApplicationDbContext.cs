37474043c0561d7531777e0a2ddedf25fdfd2b80
ahmed break <ahmed_break8@yahoo.com>
2021-09-22 14:19:29 +0200

    Create Identity DbContext

---
+++ Asp.netCore-Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Asp.netCore_Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.netCore_Identity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole,
+                                                          IdentityUserLogin<int>, IdentityRoleClaim<int>,
+                                                          IdentityUserToken<int>>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<UserRole>().HasKey(k => new {k.UserId,k.RoleId });
+            builder.Entity<User>().HasMany(u => u.UserRoles)
+                   .WithOne(u => u.User).HasForeignKey(u => u.UserId)
+                   .IsRequired();
+            builder.Entity<Role>().HasMany(r => r.UserRoles)
+                   .WithOne(r => r.Role).HasForeignKey(r => r.RoleId)
+                   .IsRequired();
+        }
+    }
+
+
+}
