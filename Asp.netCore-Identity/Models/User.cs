8b7577aca3c26901a4d7c6cd347ba33d19c2ac0e
ahmed break <ahmed_break8@yahoo.com>
2021-09-22 09:46:31 +0200

    Add Models For Identity

---
+++ Asp.netCore-Identity/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Asp.netCore_Identity.Models
+{
+    public class User : IdentityUser<int>
+    {
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
