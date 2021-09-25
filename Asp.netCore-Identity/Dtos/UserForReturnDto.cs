dcb0ba812169362f5d01f2cc5d16484f1f997dbe
ahmed break <ahmed_break8@yahoo.com>
2021-09-25 08:22:13 +0200

    Add Project DTO

---
+++ Asp.netCore-Identity/Dtos/UserForReturnDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netCore_Identity.Dtos
+{
+    public class UserForReturnDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedUserName { get; set; }
+    }
+}
