3f2295ccf56c18002fec9710c35a61600680a97a
ahmed break <ahmed_break8@yahoo.com>
2021-09-25 08:23:36 +0200

    Add AutoMapperProfiles

---
+++ Asp.netCore-Identity/Helper/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using Asp.netCore_Identity.Dtos;
+using Asp.netCore_Identity.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netCore_Identity.Helper
+{
+    public class AutoMapperProfiles:Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<UserForCreateDto, User>();
+            CreateMap<User, UserForReturnDto > ();
+        }
+    }
+}
